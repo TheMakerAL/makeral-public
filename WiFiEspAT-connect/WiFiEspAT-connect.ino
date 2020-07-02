fcac435491a520b8cefbb0501da2fd929d1a5803
Alan Zhao <alanzhaonys@gmail.com>
2020-07-02 19:30:07 -0400

    add WiFiEsp-connect and WiFiEspAT-connect examples

---
+++ WiFiEspAT-connect/WiFiEspAT-connect.ino
@@ -0,0 +1,45 @@
+#include <WiFiEspAT.h>
+
+char ssid[] = "your-ssid";
+char password[] = "your-passwordword";
+
+WiFiClient client;
+
+void setup() {
+  // Serial for debugging
+  Serial.begin(115200);
+  // Serial for ESP8266 communication
+  Serial3.begin(115200);
+  WiFi.init(Serial3);
+
+  // Connect to WiFi
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Connecting to WiFi");
+    WiFi.begin(ssid, password);
+  }
+  Serial.println("Connected to WiFi");
+
+  // Print status
+  printWifiStatus();
+}
+
+void loop() {
+  // Execute your code here
+}
+
+void printWifiStatus() {
+  // Print the SSID of the network you're attached to
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // Print your WiFi shield's IP address
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // Print the received signal strength
+  long rssi = WiFi.RSSI();
+  Serial.print("Signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
